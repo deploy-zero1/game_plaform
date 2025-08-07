1337cb77a3b49cba38c964dd5107177d996e1f36
Arthur <tangjiahao@tringame.net>
2025-08-07 11:42:41 +0800

    Initial Vercel deployment

---
+++ src/config/site.js
@@ -0,0 +1,32 @@
+
+export const siteConfig = {
+  name: "Unbanned Games Club",
+  description: "Play Unbanned Games For Free",
+  url: "https://unbanned.games",
+  ogImage: "/images/share.png",
+  links: {
+    github: "https://github.com/deploy-zero1/game_plaform",
+    twitter: "",
+    facebook: "",
+    instagram: "",
+    discord: ""
+  },
+  mainNav: [
+  {
+    "title": "Home",
+    "href": "/"
+  },
+  {
+    "title": "Games",
+    "href": "/games"
+  },
+  {
+    "title": "Blog",
+    "href": "/blog"
+  },
+  {
+    "title": "About",
+    "href": "/about"
+  }
+]
+}
